3eba24a27d66304f7e5285fdc03d6396280043ec
adarshsree11 <adarshsree11@gmail.com>
2021-07-14 16:17:37 +0530

    updated get for article route

--- app.js
+++ app.js
@@ -22,6 +22,17 @@ const articleSchema = {
 
 const Article = mongoose.model("Article", articleSchema);
 
+app.get("/articles", function(req, res){
+    Article.find({}, function(err, result){
+        if(!err){
+            res.send(result);
+        }
+        else{
+            res.send(err);
+        }
+    })
+})
+
 app.listen(3000, function(){
     console.log('server running at port 3000');
 })
