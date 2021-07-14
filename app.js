b4e3841b015bf92d152b89602319b012062a7391
adarshsree11 <adarshsree11@gmail.com>
2021-07-14 17:14:19 +0530

    added delete for articles route

--- app.js
+++ app.js
@@ -48,6 +48,17 @@ app.post("/articles", function(req, res){
     });
 })
 
+app.delete("/articles", function(req, res){
+    Article.deleteMany({}, function(err){
+        if(!err){
+            res.send("Successfully deleted all the articles!");
+        }
+        else{
+            res.send(err);
+        }
+    });
+})
+
 app.listen(3000, function(){
     console.log('server running at port 3000');
 })
